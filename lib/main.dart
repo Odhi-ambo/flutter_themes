3d1a7485ebbd3d0b861c71d3e3ec5c7490c6bb45
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-20 21:27:31 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:fl_themes/pages/home_page.dart';
+import 'package:fl_themes/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const HomePage(),
+      theme: lightMode,
+      darkTheme: darkMode,
     );
   }
 }
